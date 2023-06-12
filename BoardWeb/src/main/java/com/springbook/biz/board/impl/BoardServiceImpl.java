eb442b6e0c05bb08b1b0a0d43fdecca44403f068
byeongseokim <kbs970318@gmil.com>
2023-06-12 15:52:41 +0900

    AOP

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -14,22 +14,27 @@ public class BoardServiceImpl implements BoardService {
 	private BoardDAO boardDAO;
 
 	public void insertBoard(BoardVO vo) {
+
 		boardDAO.insertBoard(vo);
 	}
 
 	public void updateBoard(BoardVO vo) {
+
 		boardDAO.updateBoard(vo);
 	}
 
 	public void deleteBoard(BoardVO vo) {
+
 		boardDAO.deleteBoard(vo);
 	}
 
 	public BoardVO getBoard(BoardVO vo) {
+
 		return boardDAO.getBoard(vo);
 	}
 
 	public List<BoardVO> getBoardList(BoardVO vo) {
+
 		return boardDAO.getBoardList(vo);
 	}
 }
---
+++ BoardWeb/src/main/java/com/springbook/biz/common/Log4jAdvice.java
@@ -0,0 +1,8 @@
+package com.springbook.biz.common;
+
+public class Log4jAdvice {
+	public void printLog() {
+		System.out.println("[공통 로그-Log4j] 비즈니스 로직 수행 전 동작");
+	}
+
+}

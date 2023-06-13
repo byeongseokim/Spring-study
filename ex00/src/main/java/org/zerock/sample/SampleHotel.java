3cb90206eccfdd28ed77277014b173dcdbec16ef
byeongseokim <kbs970318@gmil.com>
2023-06-13 16:01:12 +0900

    의존성 주입

---
+++ ex00/src/main/java/org/zerock/sample/SampleHotel.java
@@ -0,0 +1,20 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@AllArgsConstructor
+public class SampleHotel {
+	
+	
+	private Chef chef;
+
+	
+
+}

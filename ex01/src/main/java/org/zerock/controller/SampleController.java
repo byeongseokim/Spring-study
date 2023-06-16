a9535ce1bf55a0377c8021aee56304432396c0aa
byeongseokim <kbs970318@gmil.com>
2023-06-16 16:16:35 +0900

    MVC Controller / RequestMapping

---
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,42 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.SampleDTO;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+
+	@RequestMapping("")
+	public void basic() {
+		log.info("basic.............");
+	}
+
+	@GetMapping("/basicOnlyGet")
+	public void basicGet2() {
+
+		log.info("basic get only get...............");
+	}
+
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+
+		log.info("" + dto);
+		return "ex01";
+	}
+
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+
+		log.info("name: " + name);
+		log.info("age: " + age);
+		
+		return "ex02";
+	}
+}

1e3ce88b6a782e0a70db770b57cf99667de7617e
byeongseokim <kbs970318@gmil.com>
2023-06-21 14:19:58 +0900

    VO/Mapper 완료 // DAO (X)

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -13,4 +13,10 @@ public interface BoardMapper {
 
 	public void insertSelectKey(BoardVO board);
 
+	public BoardVO read(long bno);
+	
+	public int delete(long bno);
+	
+	public int update(BoardVO board);
+
 }
---
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,58 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+	
+	private BoardMapper mapper;
+
+	@Override
+	public void register(BoardVO board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BoardMapper getMapper() {
+		return mapper;
+	}
+
+	public void setMapper(BoardMapper mapper) {
+		this.mapper = mapper;
+	}
+	
+
+}

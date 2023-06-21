ef2691ff6addc35b8dc62c16ac0fcdfee8881d82
byeongseokim <kbs970318@gmil.com>
2023-06-21 14:17:14 +0900

    DAO/Mapper 완료

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -8,32 +8,75 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
 
 import lombok.extern.log4j.Log4j;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class BoardMapperTests {
-	
+
 	@Autowired
 	private BoardMapper mapper;
-	
+
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
 	}
-	
+
 	@Test
 	public void testInsert() {
-		
+
 		BoardVO board = new BoardVO();
 		board.setTitle("새로 작성하는 글");
 		board.setContent("새로 작성하는 내용");
-		board.setWriter("newbie");;
-		
+		board.setWriter("newbie");
+		;
+
 		mapper.insert(board);
-		
+
 		log.info(board);
 	}
-	
-	
 
+	@Test
+	public void testInsertSelectKey() {
+
+		BoardVO board = new BoardVO();
+		board.setTitle(null);
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("newbie");
+		;
+
+		mapper.insertSelectKey(board);
+
+		log.info(board);
+	}
+
+	@Test
+	public void testRead() {
+
+		// 존재하는 게시물 번호로 테스트
+		BoardVO board = mapper.read(5L);
+
+		log.info(board);
+	}
+
+	@Test
+	public void testDelete() {
+
+		log.info("DELETE COUNT: " + mapper.delete(15L));
+	}
+
+	@Test
+	public void testUpdate() {
+
+		BoardVO board = new BoardVO();
+		// 실행전 존재하는 번호인지 학인할 것
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
+	}
 }

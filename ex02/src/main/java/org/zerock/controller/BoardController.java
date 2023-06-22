2f1a1c715911e3415d8567ec6ed288db6dd54ae3
byeongseokim <kbs970318@gmil.com>
2023-06-22 16:25:15 +0900

    BootStrap을 이용한 화면처리

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -3,7 +3,11 @@ package org.zerock.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -18,10 +22,47 @@ public class BoardController {
 	private BoardService service;
 
 	@GetMapping("/list")
-	public void list(Model model) {
+	public String list(Model model) {
 
 		log.info("list");
 		model.addAttribute("list", service.getList());
+		return "board/list";
+	}
+
+	// POST 방식으로 처리
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+
+		log.info("reguster: " + board);
+
+		service.register(board);
+
+		rttr.addFlashAttribute("result", board.getBno());
+
+		return "redirect:/board/list";
+		// redirect: 접두어를 사용하면 MVC가 내부적으로 response.sendRedirect()를 처리해 주기 때문에 편리함
+	}
+
+	// get방식으로 접근 하지만 post 방식으로 동작하므로 @postMapping을 이용해서 처리
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify: " + board);
+
+		if (service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	// 삭제는 반드시 POST 방식으로만 처리함
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+
+		log.info("remove..." + bno);
+		if (service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
 	}
 
 }
--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -1,7 +1,5 @@
 package org.zerock.controller;
 
-
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,6 +39,45 @@ public class BoardControllerTests {
 		log.info(
 				mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap());
 
+	}
+
+	@Test
+	public void testRegister() throws Exception {
+
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/register")
+						.param("title", "테스트 새글 제목")
+						.param("content", "테스트 새글 내용")
+						.param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+
+		log.info(resultPage);
+	}
+
+	//service.modify는 수정 여부를 boolean으로 처리하므로 이를 이용해서 성공한 경우에만 RedirectAttribute에 추가함
+	@Test
+	public void testModify() throws Exception {
+
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/modify")
+						.param("bno", "1")
+						.param("title", "수정된 테스트 새글 제목")
+						.param("content", "수정된 테스트 새글 내용")
+						.param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+
+		log.info(resultPage);
+	}
+
+	//삭제후 페이지 이동이 필요하므로 RedirectAttributes를 파라미터로 사용하고 redirect를 이용해서 삭제후 다시 목록 페이지로 이동함
+	@Test
+	public void testRemove() throws Exception {
+		//삭제전 데이터베이스에 게시물 번호 확인할 것
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+				.param("bno", "18")).andReturn()  //bno 18번 삭제 테스트
+				.getModelAndView().getViewName();
+
+		log.info(resultPage);
 	}
 
 }
